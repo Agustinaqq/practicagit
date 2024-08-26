a50119e5f3aac03465f94fd07269aa112c78d993
danpinn24 <danapinna@gmail.com>
2024-08-26 19:42:10 -0300

    Codigo 3 de practica

---
+++ codigo3.js
@@ -0,0 +1,14 @@
+/*Declare un nombre de función printArray. Toma matriz como parámetro e imprime cada va-
+lor de la matriz.*/
+
+function printArray(array) {
+    //for (let i = 0; i < array.length; i++) {
+      //  console.log (array[i]) }
+        console.log(array)  }   
+
+printArray([["dana"] ,true ,[12457],"abc",["tita"]])
+
+let frutas = ["Manzana", "Banana"];
+
+console.log(frutas);
+frutas.forEach
